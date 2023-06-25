0ac3867de54a5324323ea7ac118e1928a3d5457a
giremunga <134525119+giremunga@users.noreply.github.com>
2023-06-25 17:56:01 +0300

    added members

---
+++ Coders-101/src/Members.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Members = () => {
+  return (
+    <div>
+      <h1>The group members are four in number</h1>
+      <p>they include</p>
+      <ul>
+        <li>LIZ</li>
+        <li>GRACE</li>
+        <li>HEZRON</li>
+        <li>NATHAN</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Members
